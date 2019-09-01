d805550da6b9140b46aaaae9edcf44f99fe68e6c
zorbyte <zorbytee@gmail.com>
2019-09-01 14:08:07 +1000

    gw + types: Add resuming, work on types.

--- src/Gateway/WebsocketShard.ts
+++ src/Gateway/WebsocketShard.ts
@@ -6,68 +6,104 @@ import {
 } from "https://deno.land/std@v0.16.0/ws/mod.ts";
 import createDebug from "https://deno.land/x/debuglog/debug.ts";
 import { GATEWAY_URI } from "../lib/constants.ts";
-import { GatewayStatus, OP_CODES, GatewayPacket } from "../types.ts";
+import { Gateway } from "../@types/dencord.ts";
 import Client from "../Client.ts";
+import { UnexpectedEOFError } from "../../../../../Library/Caches/deno/deps/https/deno.land/std@v0.16.0/io/bufio.ts";
 
 const debug = createDebug("dencord:WebsocketShard");
 
 class WebsocketShard {
   public socket!: WebSocket;
-  public status: GatewayStatus = "connecting";
+  public status: Gateway.GatewayStatus = "connecting";
   private heartbeat?: number;
   private heartbeatAck = false;
+  private failedHeartbeatAck = 0;
   private seq: number | null = null;
+  private sessionID?: string;
 
   public constructor(private token: string, private client: Client) { }
 
   public async connect(): Promise<void> {
     try {
-      await this.connectWs();
+      this.socket = await connectWebSocket(GATEWAY_URI);
+      await this.onOpen();
       for await (const payload of this.socket.receive()) {
         if (typeof payload === "string") {
           const packet = JSON.parse(payload);
           await this.handlePacket(packet);
-          if (packet.op === OP_CODES.DISPATCH) this.client.emit(packet.t, packet.d);
         } else if (isWebSocketCloseEvent(payload)) {
-          this.onClose(payload);
+          this.client.removeAllListeners();
+          await this.onClose(payload);
           break;
         }
       }
     } catch (err) {
       if (this.socket) this.close(1011);
-      throw err;
+      // Likely lost internet connection.
+      if (err instanceof UnexpectedEOFError) {
+        this.status = "resuming";
+        await this.connect();
+        return;
+      } else throw err;
     }
   }
 
-  private async connectWs(): Promise<void> {
-    this.socket = await connectWebSocket(GATEWAY_URI);
-    await this.onOpen();
-  }
-
   private async onOpen(): Promise<void> {
+    let isResuming = this.status === "resuming";
     this.status = "handshaking";
     debug("Started handshaking.");
     await this.sendHeartbeat();
     await this.identifyClient();
+    if (isResuming) {
+      await this.send(Gateway.OP_CODES.RESUME, {
+        token: this.token,
+        session_id: this.sessionID,
+        seq: this.seq,
+      });
+    }
   }
 
-  private onClose(closeData: WebSocketCloseEvent): void {
+  private async onClose(closeData: WebSocketCloseEvent): Promise<void | never> {
+    await this.close();
     debug(`Disconnected with code ${closeData.code} for reason:\n${closeData.reason}.`);
     this.status = "disconnected";
+    const {
+      UNKNOWN_ERROR,
+      INVALID_SEQ,
+      RATE_LIMITED,
+      SESSION_TIMEOUT
+    } = Gateway.CLOSE_CODES;
+    if (
+      this.sessionID
+      && closeData.code === UNKNOWN_ERROR
+      || closeData.code === INVALID_SEQ
+      || closeData.code === RATE_LIMITED
+      || closeData.code === SESSION_TIMEOUT
+    ) {
+      this.status = "resuming";
+      await this.connect();
+    }
   }
 
-  private async handlePacket(packet: GatewayPacket): Promise<void> {
+  private async handlePacket(packet: Gateway.GatewayPacket): Promise<void | never> {
     this.seq = packet.s;
-    if (packet.op === OP_CODES.HELLO) {
+    if (packet.op === Gateway.OP_CODES.HELLO) {
       this.setHeartbeat(packet.d.heartbeat_interval);
-    } else if (packet.op === OP_CODES.HEARTBEAT_ACK) {
+    } else if (packet.op === Gateway.OP_CODES.HEARTBEAT_ACK) {
       this.heartbeatAck = true;
       debug("Received heartbeat ack.");
+    } else if (packet.op === Gateway.OP_CODES.RECONNECT) {
+      await this.close();
+      await this.connect();
     }
-    
-    if (packet.op === OP_CODES.DISPATCH) {
-      if (packet.t === "READY") this.status = "ready";
+  
+    if (packet.op === Gateway.OP_CODES.DISPATCH) {
+      if (packet.t === "READY") {
+        this.status = "ready";
+        this.sessionID = packet.d.session_id;
+      }
       debug(`Received dispatch event: ${packet.t}.`);
+      this.client.emit(packet.t as Gateway.DispatchEvents, packet.d);
     }
   }
 
@@ -77,7 +113,7 @@ class WebsocketShard {
     this.heartbeat = setInterval(this.sendHeartbeat.bind(this), interval);
   }
 
-  private send(op: OP_CODES, data: any): Promise<void> {
+  private send(op: Gateway.OP_CODES, data: any): Promise<void> {
     return this.socket.send(JSON.stringify({
       op,
       d: data,
@@ -86,21 +122,33 @@ class WebsocketShard {
 
   private async sendHeartbeat(): Promise<void> {
     debug(`Sending heartbeat.`);
-    if (!this.heartbeatAck && this.status !== "handshaking") {
+    if (!this.heartbeatAck) {
+      this.failedHeartbeatAck++;
       debug("Did not receive heartbeat ACK before next heartbeat!");
-      return this.close(1014);
+      if (this.status === "ready") {
+        this.status = "resuming";
+        await this.close();
+        await this.connect();
+      } else if (this.status === "handshaking" && this.failedHeartbeatAck > 2) {
+        await this.close(1014);
+        this.client.removeAllListeners();
+        throw new Error("Failed to receive heartbeat after 3 attempts!")
+      }
     }
+
     this.heartbeatAck = false;
-    await this.send(OP_CODES.HEARTBEAT, this.seq);
+    await this.send(Gateway.OP_CODES.HEARTBEAT, this.seq);
   }
 
-  public close(code = 1000): Promise<void> {
-    return this.socket.close(code);
+  public async close(code = 1000): Promise<void> {
+    this.failedHeartbeatAck = 0;
+    if (this.heartbeat) clearInterval(this.heartbeat);
+    if (!this.socket.isClosed) this.socket.close(code);
   }
 
   private identifyClient(): Promise<void> {
     debug("Identifying client.");
-    return this.send(OP_CODES.IDENTIFY, {
+    return this.send(Gateway.OP_CODES.IDENTIFY, {
       token: this.token,
       properties: {
         $os: Deno.platform.os,
