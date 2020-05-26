83aefe4c504f80f099be0edc9c8bf1dcdd543465
TTtie <tttiett@outlook.com>
2020-05-26 13:09:39 +0200

    Port the old REST and WS API

--- bin/test_new_api.ts
+++ bin/test_new_api.ts
@@ -1,13 +1,27 @@
 import { config, login, onDebug, onError } from "../mod.ts";
-import { on } from "../ws.ts";
+import { on, configure as wsConfigure, CompressionOptions } from "../ws.ts";
+import rest from "../rest.ts";
+import cfg from "./testConfig.ts";
 
 config({ someOption: false });
+wsConfigure({
+  compress: CompressionOptions.ZLIB_STREAM
+})
 
 onDebug(console.debug);
 onError(console.error);
 
-on("message", msg => {
-  console.log(msg);
+on("MESSAGE_CREATE", async msg => {
+  //console.log(msg);
+  if ((<{
+    content: string;
+  }>msg).content === "deno!hello") {
+    await rest.request("POST", `/channels/${(<{
+      channel_id: string;
+    }>msg).channel_id}/messages`, true, {
+      content: "Hi!"
+    });
+  }
 });
 
-await login("A_TOKEN");
+await login(cfg.token);
