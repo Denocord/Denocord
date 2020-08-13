54d4b6e341fca9d9a87cd36bfe8ccd1335eda839
TTtie <tttiett@outlook.com>
2020-08-14 00:23:36 +0200

    Update std

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,12 +1,12 @@
 export { default as StrictEventEmitter } from "https://raw.githubusercontent.com/bterlson/strict-event-emitter-types/master/src/index.ts";
-export { default as EventEmitter } from "https://deno.land/std@v0.62.0/node/events.ts";
+export { default as EventEmitter } from "https://deno.land/std@v0.65.0/node/events.ts";
 export {
   connectWebSocket,
   isWebSocketCloseEvent,
   WebSocket,
   WebSocketCloseEvent,
-} from "https://deno.land/std@v0.62.0/ws/mod.ts";
-export { equal } from "https://deno.land/std@v0.62.0/bytes/mod.ts";
+} from "https://deno.land/std@v0.65.0/ws/mod.ts";
+export { equal } from "https://deno.land/std@v0.65.0/bytes/mod.ts";
 export { default as pako } from "https://raw.githubusercontent.com/Denocord/pako/master/mod.js";
 export { default as decompressor } from "https://raw.githubusercontent.com/Denocord/denoflate/3cb89c23c4cbd6b2609113749d58248c8921537a/mod.ts";
 export * as APITypes from "https://raw.githubusercontent.com/Denocord/discord-api-types/05f0a60d20ab769cb8139beddf131d269fbda046/src/high_level.ts";
