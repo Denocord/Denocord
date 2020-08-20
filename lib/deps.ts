20e00c2d4b8a4d50144afb61c88163969efa4920
TTtie <tttiett@outlook.com>
2020-08-20 20:48:51 +0200

    Stop using deprecated version specifiers

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,12 +1,12 @@
 export { default as StrictEventEmitter } from "https://raw.githubusercontent.com/bterlson/strict-event-emitter-types/master/src/index.ts";
-export { default as EventEmitter } from "https://deno.land/std@v0.65.0/node/events.ts";
+export { default as EventEmitter } from "https://deno.land/std@0.65.0/node/events.ts";
 export {
   connectWebSocket,
   isWebSocketCloseEvent,
   WebSocket,
   WebSocketCloseEvent,
-} from "https://deno.land/std@v0.65.0/ws/mod.ts";
-export { equal } from "https://deno.land/std@v0.65.0/bytes/mod.ts";
+} from "https://deno.land/std@0.65.0/ws/mod.ts";
+export { equal } from "https://deno.land/std@0.65.0/bytes/mod.ts";
 export { default as pako } from "https://raw.githubusercontent.com/Denocord/pako/master/mod.js";
 export { default as decompressor } from "https://raw.githubusercontent.com/Denocord/denoflate/7b8c03e4b34517860a106a515c3c9565cf8743f3/mod.ts";
 //deno-fmt-ignore-line
