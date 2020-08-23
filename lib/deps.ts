3ebc40764bad179b62cfe776cff65d5944e2bf6d
TTtie <tttiett@outlook.com>
2020-08-23 20:52:02 +0200

    Use denoflate v0.5.0

--- lib/deps.ts
+++ lib/deps.ts
@@ -8,7 +8,7 @@ export {
 } from "https://deno.land/std@0.65.0/ws/mod.ts";
 export { equal } from "https://deno.land/std@0.65.0/bytes/mod.ts";
 export { default as pako } from "https://raw.githubusercontent.com/Denocord/pako/master/mod.js";
-export { default as decompressor } from "https://raw.githubusercontent.com/Denocord/denoflate/7b8c03e4b34517860a106a515c3c9565cf8743f3/mod.ts";
+export { default as decompressor } from "https://raw.githubusercontent.com/Denocord/denoflate/66b73416e81f852ba7982bf67eb0e276b2f2bd0b/mod.ts";
 //deno-fmt-ignore-line
 //export { default as decompressor } from "../../denoflate/mod.ts";
 export * as APITypes from "https://raw.githubusercontent.com/Denocord/discord-api-types/05f0a60d20ab769cb8139beddf131d269fbda046/src/high_level.ts";
