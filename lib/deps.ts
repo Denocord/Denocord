62e39d3872173c30393281a169cb1dedb9a7499f
TTtie <tttiett@outlook.com>
2020-09-13 01:02:24 +0200

    ⬆️ (std): 0.68.0

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,12 +1,12 @@
 export { default as StrictEventEmitter } from "https://raw.githubusercontent.com/bterlson/strict-event-emitter-types/master/src/index.ts";
-export { default as EventEmitter } from "https://deno.land/std@0.66.0/node/events.ts";
+export { default as EventEmitter } from "https://deno.land/std@0.68.0/node/events.ts";
 export {
   connectWebSocket,
   isWebSocketCloseEvent,
   WebSocket,
   WebSocketCloseEvent,
-} from "https://deno.land/std@0.66.0/ws/mod.ts";
-export { equal } from "https://deno.land/std@0.66.0/bytes/mod.ts";
+} from "https://deno.land/std@0.68.0/ws/mod.ts";
+export { equal } from "https://deno.land/std@0.68.0/bytes/mod.ts";
 export { inflate } from "https://deno.land/x/zlib.es@v1.0.0/mod.ts";
 export { default as decompressor } from "https://raw.githubusercontent.com/Denocord/denoflate/6319461e7203631cc73abbefcb0306a14059f7ab/mod.ts";
 //deno-fmt-ignore-line
