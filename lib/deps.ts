1cb937993ed02d27168cb00269c35cc73aa54863
TTtie <tttiett@outlook.com>
2020-09-12 21:29:07 +0200

    ⬆️ (discord-api-types): c6cbd8e15fa0afd5f3b50640fa5802822994c345

--- lib/deps.ts
+++ lib/deps.ts
@@ -11,4 +11,4 @@ export { default as pako } from "https://raw.githubusercontent.com/Denocord/pako
 export { default as decompressor } from "https://raw.githubusercontent.com/Denocord/denoflate/e2c3812d8410abc1f2c0b200e5e89f42ee1e907e/mod.ts";
 //deno-fmt-ignore-line
 //export { default as decompressor } from "../../denoflate/mod.ts";
-export * as APITypes from "https://raw.githubusercontent.com/Denocord/discord-api-types/c5f9129ae8bb8ec7f3b3f1ec8edce8bb5887f1a4/src/high_level.ts";
+export * as APITypes from "https://raw.githubusercontent.com/Denocord/discord-api-types/c6cbd8e15fa0afd5f3b50640fa5802822994c345/src/high_level.ts";
