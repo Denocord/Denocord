e3396dfcea1d6ae46418fcbcf408537e3d1c6c18
TTtie <tttiett@outlook.com>
2020-09-15 16:53:29 +0200

    ⬆️ (discord-api-types-new): 554bb77056895a73c87fce7dfadcfa33a158bbdb

--- lib/deps.ts
+++ lib/deps.ts
@@ -7,4 +7,4 @@ export { default as decompressor } from "https://raw.githubusercontent.com/Denoc
 //export { default as decompressor } from "../../denoflate/mod.ts";
 
 // TODO: rewrite from discord-api-types-new to discord-api-types once main repo renamed
-export * as APITypes from "https://raw.githubusercontent.com/Denocord/discord-api-types-new/f33cb0146707cbecfe9ab670a9493881b856d170/high_level.ts";
+export * as APITypes from "https://raw.githubusercontent.com/Denocord/discord-api-types-new/554bb77056895a73c87fce7dfadcfa33a158bbdb/high_level.ts";
