1da5235af88e5fed12f0924350be6692051e62ac
TTtie <tttiett@outlook.com>
2020-09-20 15:59:27 +0200

    ⬆️ (std): 0.70.0

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,6 +1,6 @@
 export type { default as StrictEventEmitter } from "https://raw.githubusercontent.com/bterlson/strict-event-emitter-types/master/src/index.ts";
-export { default as EventEmitter } from "https://deno.land/std@0.69.0/node/events.ts";
-export { equal } from "https://deno.land/std@0.69.0/bytes/mod.ts";
+export { default as EventEmitter } from "https://deno.land/std@0.70.0/node/events.ts";
+export { equal } from "https://deno.land/std@0.70.0/bytes/mod.ts";
 export { inflate } from "https://deno.land/x/zlib.es@v1.0.0/mod.ts";
 export { default as decompressor } from "https://raw.githubusercontent.com/Denocord/denoflate/d252d766d90ff4b9d5dde6831367a051b343aaea/mod.ts";
 //deno-fmt-ignore-line
